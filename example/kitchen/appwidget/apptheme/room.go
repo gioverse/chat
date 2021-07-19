a587ac6fa6053c51b51a4083f269c09a4ecf08ce
Jack Mordaunt <jackmordaunt.dev@gmail.com>
2021-07-19 19:38:24 +0800

    example/kitchen/appwidget/apptheme: make RoomStyle theme-aware
    Handle dark theme by making the overlay a function of theme.Fg with
    reduced alpha.
    
    Signed-off-by: Jack Mordaunt <jackmordaunt.dev@gmail.com>

--- example/kitchen/appwidget/apptheme/room.go
+++ example/kitchen/appwidget/apptheme/room.go
@@ -11,6 +11,7 @@ import (
 	"gioui.org/x/component"
 	"git.sr.ht/~gioverse/chat/example/kitchen/appwidget"
 	"git.sr.ht/~gioverse/chat/example/kitchen/model"
+	chatlayout "git.sr.ht/~gioverse/chat/layout"
 	matchat "git.sr.ht/~gioverse/chat/widget/material"
 )
 
@@ -22,6 +23,7 @@ type RoomStyle struct {
 	Summary   material.LabelStyle
 	TimeStamp material.LabelStyle
 	Indicator color.NRGBA
+	Overlay   color.NRGBA
 }
 
 // Room creates a style type that can lay out the data for a room.
@@ -49,65 +51,62 @@ func Room(th *material.Theme, interact *appwidget.Room, room *model.Room) RoomSt
 			Width:  unit.Dp(25),
 		},
 		Indicator: th.ContrastBg,
+		Overlay:   component.WithAlpha(th.Fg, 50),
 	}
 }
 
 func (room RoomStyle) Layout(gtx C) D {
-	dims := material.Clickable(gtx, &room.Clickable, func(gtx C) D {
-		return layout.UniformInset(unit.Dp(8)).Layout(gtx, func(gtx C) D {
-			return layout.Flex{
-				Axis:      layout.Horizontal,
-				Alignment: layout.Middle,
-			}.Layout(
-				gtx,
-				layout.Rigid(func(gtx C) D {
-					return room.Image.Layout(gtx)
-				}),
-				layout.Rigid(layout.Spacer{Width: unit.Dp(5)}.Layout),
-				layout.Flexed(1, func(gtx C) D {
-					return layout.Flex{
-						Axis: layout.Vertical,
-					}.Layout(
-						gtx,
-						layout.Rigid(func(gtx C) D {
-							return room.Name.Layout(gtx)
-						}),
-						layout.Rigid(layout.Spacer{Height: unit.Dp(5)}.Layout),
-						layout.Rigid(func(gtx C) D {
-							return component.TruncatingLabelStyle(room.Summary).Layout(gtx)
-						}),
-					)
-				}),
-				layout.Rigid(layout.Spacer{Width: unit.Dp(5)}.Layout),
-				layout.Rigid(func(gtx C) D {
-					return room.TimeStamp.Layout(gtx)
-				}),
-			)
+	var (
+		surface = func(gtx C, w layout.Widget) D { return w(gtx) }
+		dims    layout.Dimensions
+	)
+	if room.Active {
+		surface = chatlayout.Background(room.Overlay).Layout
+		defer func() {
+			// Close-over the dimensions and layout the indicator atop everything
+			// else.
+			component.Rect{
+				Size: image.Point{
+					X: gtx.Px(unit.Dp(3)),
+					Y: dims.Size.Y,
+				},
+				Color: room.Indicator,
+			}.Layout(gtx)
+		}()
+	}
+	dims = surface(gtx, func(gtx C) D {
+		return material.Clickable(gtx, &room.Clickable, func(gtx C) D {
+			return layout.UniformInset(unit.Dp(8)).Layout(gtx, func(gtx C) D {
+				return layout.Flex{
+					Axis:      layout.Horizontal,
+					Alignment: layout.Middle,
+				}.Layout(
+					gtx,
+					layout.Rigid(func(gtx C) D {
+						return room.Image.Layout(gtx)
+					}),
+					layout.Rigid(layout.Spacer{Width: unit.Dp(5)}.Layout),
+					layout.Flexed(1, func(gtx C) D {
+						return layout.Flex{
+							Axis: layout.Vertical,
+						}.Layout(
+							gtx,
+							layout.Rigid(func(gtx C) D {
+								return room.Name.Layout(gtx)
+							}),
+							layout.Rigid(layout.Spacer{Height: unit.Dp(5)}.Layout),
+							layout.Rigid(func(gtx C) D {
+								return component.TruncatingLabelStyle(room.Summary).Layout(gtx)
+							}),
+						)
+					}),
+					layout.Rigid(layout.Spacer{Width: unit.Dp(5)}.Layout),
+					layout.Rigid(func(gtx C) D {
+						return room.TimeStamp.Layout(gtx)
+					}),
+				)
+			})
 		})
 	})
-	if room.Active {
-		layout.Flex{}.Layout(
-			gtx,
-			layout.Rigid(func(gtx C) D {
-				return component.Rect{
-					Size: image.Point{
-						X: gtx.Px(unit.Dp(3)),
-						Y: dims.Size.Y,
-					},
-					Color: room.Indicator,
-				}.Layout(gtx)
-
-			}),
-			layout.Rigid(func(gtx C) D {
-				return component.Rect{
-					Size: image.Point{
-						X: gtx.Constraints.Max.X,
-						Y: dims.Size.Y,
-					},
-					Color: color.NRGBA{A: 50},
-				}.Layout(gtx)
-			}),
-		)
-	}
 	return dims
 }
