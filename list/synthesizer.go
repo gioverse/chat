65a288a4484d78fc6c9b0a5db3dff41ed771f5fd
Chris Waldon <christopher.waldon.dev@gmail.com>
2021-08-23 15:37:41 -0400

    list: redefine synthesis as a pure function
    This commit adds the list.Synthesize function, which transforms
    a slice of list.Elements using a list.Synthesizer.
    
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

---
+++ list/synthesizer.go
@@ -0,0 +1,73 @@
+package list
+
+import "gioui.org/layout"
+
+// Synthesis holds the results of transforming a slice of Elements
+// with a Synthesizer hook.
+type Synthesis struct {
+	// Elements holds the resulting elements.
+	Elements []Element
+	// ToSourceIndicies maps each index in Elements to the index of
+	// the element that generated it when given to the Synthesizer.
+	// It is always true that Elements[i] was synthesized from
+	// Source[ToSourceIndicies[i]].
+	ToSourceIndicies []int
+	// The source elements.
+	Source []Element
+}
+
+// SerialAt returns the serial at the given index within the Source slice,
+// if there is one.
+func (s Synthesis) SerialAt(index int) Serial {
+	if index < 0 || index >= len(s.Source) {
+		return NoSerial
+	}
+	return s.Source[index].Serial()
+}
+
+// ViewportToSerials converts the First and Count fields of the provided
+// viewport into a pair of serials representing the range of elements
+// visible within that viewport.
+func (s Synthesis) ViewportToSerials(viewport layout.Position) (Serial, Serial) {
+	if len(s.ToSourceIndicies) < 1 {
+		return NoSerial, NoSerial
+	}
+	startSrcIdx := s.ToSourceIndicies[viewport.First]
+	startSerial := SerialAtOrBefore(s.Source, startSrcIdx)
+	lastIndex := len(s.ToSourceIndicies) - 1
+	endSrcIdx := s.ToSourceIndicies[min(viewport.First+viewport.Count, lastIndex)]
+	endSerial := SerialAtOrAfter(s.Source, endSrcIdx)
+	return startSerial, endSerial
+}
+
+// Synthesize applies a Synthesizer to a slice of elements, returning
+// the resulting slice of elements as well as a mapping from the index
+// of each resulting element to the input element that generated it.
+func Synthesize(elements []Element, synth Synthesizer) Synthesis {
+	var s Synthesis
+	s.Source = elements
+	for i, elem := range elements {
+		var (
+			previous Element
+			next     Element
+		)
+		if i > 0 {
+			previous = elements[i-1]
+		} else {
+			previous = Start{}
+		}
+		if i < len(elements)-1 {
+			next = elements[i+1]
+		} else {
+			next = End{}
+		}
+		synthesized := synth(previous, elem, next)
+		// Mark that each of these synthesized elements came from the
+		// raw element at index i.
+		for range synthesized {
+			s.ToSourceIndicies = append(s.ToSourceIndicies, i)
+		}
+		s.Elements = append(s.Elements, synthesized...)
+	}
+	return s
+}
